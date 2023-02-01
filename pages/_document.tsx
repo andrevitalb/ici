9a7728c22cdc705dbe0f8aae3438aebf968a97b0
André Vital <andre@andrevital.com>
2023-02-01 12:18:21 -0600

    chore: Initial repo setup (#1)

---
+++ pages/_document.tsx
@@ -0,0 +1,67 @@
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript
+} from "next/document"
+import { ServerStyleSheet } from "styled-components"
+
+export default class CustomDocument extends Document {
+	static async getInitialProps(ctx: DocumentContext) {
+		const sheet = new ServerStyleSheet()
+		const originalRenderPage = ctx.renderPage
+
+		try {
+			ctx.renderPage = () =>
+				originalRenderPage({
+					enhanceApp: (App) => (props) =>
+						sheet.collectStyles(<App {...props} />),
+				})
+
+			const initialProps = await Document.getInitialProps(ctx)
+			return {
+				...initialProps,
+				styles: (
+					<>
+						{initialProps.styles}
+						{sheet.getStyleElement()}
+					</>
+				),
+			}
+		} finally {
+			sheet.seal()
+		}
+	}
+
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link rel="canonical" href="https://www.andrevital.com/" />
+
+					{/* FontAwesome */}
+					<link rel="stylesheet" href="/css/all.min.css" />
+
+					{/* Google Fonts */}
+					<link
+						rel="preconnect"
+						href="https://fonts.googleapis.com"
+					/>
+					<link
+						rel="preconnect"
+						href="https://fonts.gstatic.com"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;600&family=Space+Grotesk:wght@400;500;600;700&display=swap"
+						rel="stylesheet"
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		)
+	}
+}
