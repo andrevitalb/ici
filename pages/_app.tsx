532d261568034d95ec625b8104a2a4deae449e3c
André Vital <andre@andrevital.com>
2023-02-01 12:11:41 -0600

    Initial NextJs files

---
+++ pages/_app.tsx
@@ -0,0 +1,18 @@
+import { ComponentType } from "react"
+import "styles/globals.css"
+import { GlobalStyles } from "twin.macro"
+
+export default function App<Props extends JSX.IntrinsicAttributes>({
+	Component,
+	pageProps,
+}: {
+	Component: ComponentType<Props>
+	pageProps: Props
+}) {
+	return (
+    <>
+			<GlobalStyles />
+			<Component {...pageProps} />
+      </>
+	)
+}
