8a271a4a32fe47c2897c85ef39775ec02fac857e
André Vital <andre@andrevital.com>
2023-02-08 08:02:05 -0600

    Created base page files

---
+++ pages/login.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+const Login = () => {
+	return (
+		<div>
+			<Head>
+				<title>Login | ICI</title>
+			</Head>
+			<div></div>
+		</div>
+	)
+}
+
+export default Login
