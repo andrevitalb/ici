3c5b58ce044715113a2f5ab06c1b724b995150ed
André Vital <andre@andrevital.com>
2023-02-09 18:30:45 -0600

    Update styling files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,18 @@
 const twColors = require("tailwindcss/colors")
 const _ = require("lodash")
 
-const colors = _.pick(twColors, ["gray", "red", "green", "black", "white"])
+const colors = _.pick(twColors, ["gray", "green", "black", "white"])
 
 const customColors = {
 	...colors,
-	gray: {
-		100: "#EBEBEB",
-		200: "#999999",
-		300: "#212121",
-		400: "#171717",
-		500: "#0E0E0E",
-	},
-	aqua: {
-		100: "#CDFFF5",
-		200: "#91FFE9",
-		300: "#64FFE1",
-		400: "#39FFD8",
-		500: "#09FBCC",
-	},
 	blue: {
-		100: "#293D5A",
-		200: "#172A45",
-		300: "#0A192F",
-		400: "#020C1B",
-		500: "#01050B",
+		200: "#b3b3cc",
+		300: "#1946a6",
+		400: "#001f60",
+		500: "#000714",
 	},
+	yellow: "#ffa400",
+	red: "#d50037",
 }
 
 module.exports = {
@@ -38,7 +25,7 @@ module.exports = {
 	theme: {
 		extend: {
 			fontFamily: {
-				display: ["Space Grotesk", "sans-serif"],
+				display: ["Lato", "sans-serif"],
 				body: ["Source Sans Pro", "sans-serif"],
 				mono: [
 					"Menlo",
